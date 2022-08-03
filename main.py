49b80fee7f33d452298895dcf4e0ac33e886ee3f
KeldenChye <Keldenchye@gmail.com>
2022-08-03 22:47:58 +0800

    minor changes to main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import api, cash_on_hand, overheads, profit_loss
 
 def main():
+    """
+    The function will call the other respective functions providing the functions with its parameter (Conversion rate) and runs all the 
+    functions at once
+    """
     forex = api.api_function()
     cash_on_hand.coh_function(forex)
     overheads.overhead_function(forex)
