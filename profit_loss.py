47171d05627702396407899eb94c44aa8e83ebb0
KeldenChye <Keldenchye@gmail.com>
2022-08-04 15:18:33 +0800

    minor changes

--- profit_loss.py
+++ profit_loss.py
@@ -6,53 +6,57 @@ def profitloss_function(forex):
     the day that there is a decline in net profit. Meanwhile, if each day is consecutively higher, the function will return the net profit surplus
     scenario
     """
-    #empty_list to store all of the data form the CSV file
-    empty_list = []
-    pl_list = []
-    day_list = []
-    #Creating a file path to access the CSV file for Profit and Loss
-    file_path = Path.cwd()/"csv_reports"/"Profit and Loss.csv"
-    #Opening the CSV file as read mode
-    with file_path.open(mode='r', encoding='UTF-8', newline="") as file:
-        reader = csv.reader(file)
-        #Skipping the header to only read the data
-        next(reader)
-        #For loop to access the data within the csv file
-        for line in reader:
-            #Appending the data from the CSV file to the empty_list
-            empty_list.append(line)
-    #For loop to iterate over the data from the empty_list
-    for value in empty_list:
-    #Appending the data for profit and loss into pl_list using their index position
-        pl_list.append(value[4])
-    #Appending the data for the range of days into the day_list using their index position
-        day_list.append(value[0])
-    #Using list comprehension to convert the values in the list from str to int
-    pl_list_final = ([int(x) for x in pl_list])
-    #Creating an empty list(list_diff) to store the difference in net profit between each days
-    list_diff = []
-    #Creating to iterate over the range of data(net profit per day)
-    for n in range(1, len(pl_list_final)):
-    #Appending the difference between the net profit per day to list_diff, obtaining the respective values based on their index position n - (n-1)
-        list_diff.append(int(pl_list_final[n] - pl_list_final[n-1]))
-    file_path1 = Path.cwd()/"summary_report.txt"
-    with file_path1.open(mode = "a", encoding = "UTF-8", newline = "") as file:
-    #For loop to iterate over the the data for the difference in net profit per day. (In enumerate) to return the index position of the negative value to be used in the day_list.
-        for day,difference in enumerate(list_diff):
-        #If statement to identify the values by which the difference is negative
-            if difference < 0:
-        #Variable to store the negative values as positive
-                final_diff = abs(difference)
-            #Variable for the index position of the day at which the net profit is negative
-                index_day = day +1
-            #appending the decline in net profit and the day at which it occured & converting it to SGD to summary_report.txt 
-                file.write(f"\n[PROFIT DEFICIT] DAY: {day_list[index_day]}, AMOUNT: SGD{round((final_diff * forex),1)}")
-            #To continue iterating over the values by which have a profit deficit
-            else:
-                continue
-            #Otherwise the code will proceed to append the net profit surplus scenario if the difference is positive to summary_report.txt
-            if difference > 0:
-                file.write("\n[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
-            
+    try:
+    #Bypassing any error
+        #empty_list to store all of the data form the CSV file
+        empty_list = []
+        pl_list = []
+        day_list = []
+        #Creating a file path to access the CSV file for Profit and Loss
+        file_path = Path.cwd()/"csv_reports"/"Profit and Loss.csv"
+        #Opening the CSV file as read mode
+        with file_path.open(mode='r', encoding='UTF-8', newline="") as file:
+            reader = csv.reader(file)
+            #Skipping the header to only read the data
+            next(reader)
+            #For loop to access the data within the csv file
+            for line in reader:
+                #Appending the data from the CSV file to the empty_list
+                empty_list.append(line)
+        #For loop to iterate over the data from the empty_list
+        for value in empty_list:
+        #Appending the data for profit and loss into pl_list using their index position
+            pl_list.append(value[4])
+        #Appending the data for the range of days into the day_list using their index position
+            day_list.append(value[0])
+        #Using list comprehension to convert the values in the list from str to int
+        pl_list_final = ([int(x) for x in pl_list])
+        #Creating an empty list(list_diff) to store the difference in net profit between each days
+        list_diff = []
+        #Creating to iterate over the range of data(net profit per day)
+        for n in range(1, len(pl_list_final)):
+        #Appending the difference between the net profit per day to list_diff, obtaining the respective values based on their index position n - (n-1)
+            list_diff.append(int(pl_list_final[n] - pl_list_final[n-1]))
+        file_path1 = Path.cwd()/"summary_report.txt"
+        with file_path1.open(mode = "a", encoding = "UTF-8", newline = "") as file:
+        #For loop to iterate over the the data for the difference in net profit per day. (In enumerate) to return the index position of the negative value to be used in the day_list.
+            for day,difference in enumerate(list_diff):
+            #If statement to identify the values by which the difference is negative
+                if difference < 0:
+            #Variable to store the negative values as positive
+                    final_diff = abs(difference)
+                #Variable for the index position of the day at which the net profit is negative
+                    index_day = day +1
+                #appending the decline in net profit and the day at which it occured & converting it to SGD to summary_report.txt 
+                    file.write(f"\n[PROFIT DEFICIT] DAY: {day_list[index_day]}, AMOUNT: SGD{round((final_diff * forex),1)}")
+                #To continue iterating over the values by which have a profit deficit
+                else:
+                    continue
+                #Otherwise the code will proceed to append the net profit surplus scenario if the difference is positive to summary_report.txt
+                if difference > 0:
+                    file.write("\n[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
+    except TypeError:
+        print("Refer to summary_report.txt")
+    #Printing the str if a type error occurs during the conversion
 
 profitloss_function(1)
